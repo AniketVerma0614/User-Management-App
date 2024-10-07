552883b8800f43d7747536a86aaa932f01c86ff4
AniketVerma0614 <aniketverma0614@gmail.com>
2024-10-07 19:48:20 +0530

    Generate and insert 100 random users into the database
    - Established a connection to the MySQL database.
    - Created a query to insert multiple rows into the `user` table.
    - Implemented a function to generate random user data using the faker library, returning an array of user details.
    - Populated the database with 100 fake user records consisting of unique IDs, usernames, emails, and passwords.
    - Closed the database connection after the insert operation.

--- index.js
+++ index.js
@@ -14,14 +14,23 @@ const connection = mysql.createConnection({
 // Updated query to handle multiple rows of data
 let q = "INSERT INTO user (id, username, email, password) VALUES ?";
 
-// Array of multiple user data sets
-let users = [
-  ["901", "8989_newusers", "mn@gmail.com", "abc"],
-  ["671", "7878_newusers", "eg@gmail.com", "abc"]
-];
+// Function to generate random user data in array format
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),              // Generate a unique ID
+    faker.internet.userName(),        // Generate a random username
+    faker.internet.email(),           // Generate a random email
+    faker.internet.password(),         // Generate a random password
+  ];
+};
+
+let data = [];
+for (let i = 0; i < 100; i++) {
+  data.push(getRandomUser()); // Generate 100 fake users
+}
 
 try {
-  connection.query(q, [users], (err, results) => {
+  connection.query(q, [data], (err, results) => {
     if (err) throw err;
     console.log('Data inserted successfully:', results);
   });
@@ -32,43 +41,3 @@ try {
 // Close the database connection
 connection.end();
 
-// Function to generate random user data in array format
-let getRandomUser = () => {
-  return{
-    userId: faker.string.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
-};
-
-/*
-PS C:\Users\HP\OneDrive\Desktop\DELTA4\SQLCLASS> node index.js
-Data inserted successfully: ResultSetHeader {
-  fieldCount: 0,
-  affectedRows: 2,
-  insertId: 0,
-  info: 'Records: 2  Duplicates: 0  Warnings: 0',
-  serverStatus: 2,
-  warningStatus: 0,
-  changedRows: 0
-}
-*/
-
-
-
-/*
-mysql> SELECT * from user;
-+-----+---------------+---------------+----------+
-| id  | username      | email         | password |
-+-----+---------------+---------------+----------+
-| 123 | 123_newuser   | abc@gmail.com | abc      |
-| 671 | 7878_newusers | eg@gmail.com  | abc      |
-| 677 | 56_newusers   | efg@gmail.com | abc      |
-| 900 | 99_newusers   | mon@gmail.com | abc      |
-| 901 | 8989_newusers | mn@gmail.com  | abc      |
-+-----+---------------+---------------+----------+
-5 rows in set (0.00 sec)
-
-mysql>
-*/
