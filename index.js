74de534e268da4d75bfbeb8bb749ba50219328cb
AniketVerma0614 <aniketverma0614@gmail.com>
2024-10-07 16:42:44 +0530

    feat: Add database connection and data insertion logic with Faker.js support

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 //index.js
-const { faker } = require('@faker-js/faker');
+const {faker} = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+// Create the connection to database
+const connection =mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'delta_app',
+    password: "19514",
+  });
+
+//INSERTING NEW DATA !!!
+let q= "INSERT INTO user (id,username,email,password) VALUE (?,?,?,?)";
+
+let user =["123","123_newuser","abc@gmail.com","abc"];
+
+
+
+try{
+connection.query(q,user,(err,results) =>{
+    if(err) throw err;
+    console.log(results);
+});
+}
+catch(err){
+    console.log(err);
+}
+
+
+connection.end();
 
 let getRandomUser= ()=> {
-    return {
-      userId: faker.string.uuid(),
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-  }
+    return [
+      faker.string.uuid(),
+      faker.internet.userName(),
+      faker.internet.email(),
+      faker.internet.password(),
+    ];
+};
 
 
-
-console.log(getRandomUser());
 
